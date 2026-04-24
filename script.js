7cba09b5b81881ab9700194c9e3e161954722b4d
27091-lab <27091@scs.edu.do>
2026-04-24 13:05:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("dark-mode");
   }
 });
+// Animated counters
+const counters = document.querySelectorAll('.count');
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / 50;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 30);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
